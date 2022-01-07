197768b1b7a195167fb5baa4183b937eb34186dc
Oguz Demir <doguzkaann@gmail.com>
2022-01-07 18:58:21 +0300

    Added footer, menu functionality and disabled dragging images

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ console.log('hi');
 
 function main() {
     document.getElementById('hero').setAttribute('draggable', false);
+    document.getElementById('github').setAttribute('draggable', false);
+    document.getElementById('instagram').setAttribute('draggable', false);
+    document.getElementById('youtube').setAttribute('draggable', false);
 }
